8852bd1834a2e0b2e06ab3b730277188746ad296
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-09-07 01:20:35 +0530

    Refactor user registration and login logic to use Axios for API calls, improving error handling and code consistency. Update database connection method to specify database name directly. Remove unused CORS configuration in backend. Add Axios dependency to frontend.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,10 +8,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+
 
 connectDB()
 .then(()=>{
--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,115 +1,109 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
-const userSchema = new Schema({
-    fullName: {
-        type: String,
-        required: true
-    },
-    dob: {
-        type: Date
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
-    },
-    password: {
-        type: String,
-        required: [true, 'Password is required']
-    },
-    profilePhoto: {
-        type: String,
-    },
-    coverImage: {
-        type: String
-    },
-    linkedinProfile: {
-        type: String,
-    },
-    qualification: {
-        type: String,
-    },
-    skills: {
-        type: [String],
-        default: []
-    },
-    collegeName: {
-        type: String,
-    },
-    branch: {
-        type: String,
-    },
-    yearofPassing: {
-        type: String
-    },
-    experienceYears: {
-        type: String,
-        default: 0
-    },
-    age: {
-        type: Number
-    },
-    pastExperience:{
-        type: String,
-        default: 0
-    }
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        profilePhoto: {
+            type: String, // Cloudinary URL
+        },
     
-},
-{
+        githubProfile: {
+            type: String,
+        },
+        dob: {
+            type: Date,
+        },
+        qualification: {
+            type: String,
+        },
+        skills: {
+            type: [String],
+        },
+        collegeName: {
+            type: String,
+        },
+        branch: {
+            type: String,
+        },
+        experienceYears: {
+            type: Number,
+        },
+        pastExperience: {
+            type: String,
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
         timestamps: true
     }
 )
 
 userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password,10);
-    next();
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
 })
 
-userSchema.methods.isPasswordCorrect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.isPasswordValid = async function(password) {
-    return await bcrypt.compare(password, this.password);
-};
-
 userSchema.methods.generateAccessToken = function(){
-    return jwt.sign({
-        _id: this._id,
-        email: this.email,
-        username: this.username,
-        fullName: this.fullName
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-    }
-)
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
 }
-
 userSchema.methods.generateRefreshToken = function(){
-    return jwt.sign({
-        _id: this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-    }
-)
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
-
 
 export const User = mongoose.model("User", userSchema)
+
