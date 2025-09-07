da7b3bc6089ff76ada39c5ca8a98636090155290
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-09-07 22:53:48 +0530

    chore: Update CORS configuration to restrict origins
    - Modified the CORS origin setting in app.js to allow requests only from 'https://workcast-frontend.onrender.com', enhancing security by limiting access to the specified frontend application.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,9 +5,8 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: "*",
-    credentials:true
-}))
+    origin: 'https://workcast-frontend.onrender.com'
+  }));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true, limit: "16kb"}))
