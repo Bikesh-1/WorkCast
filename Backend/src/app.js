91ddc4cb23cb900d49d2e324bcfbb8dcb814beb1
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-09-07 22:58:44 +0530

    chore: Refine CORS configuration to support multiple origins
    - Updated the CORS settings in app.js to allow requests from both the environment variable specified origin and 'https://workcast-frontend.onrender.com', while enabling credentials for enhanced security and flexibility.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,8 +5,9 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: 'https://workcast-frontend.onrender.com'
-  }));
+    origin: [process.env.CORS_ORIGIN, "https://workcast-frontend.onrender.com"],
+    credentials:true
+}))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true, limit: "16kb"}))
